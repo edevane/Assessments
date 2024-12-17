c41c75b88ed284baa4bb4aa32a30f16bcb0af4e6
Edevane Tan <edevanenyc@gmail.com>
2024-12-17 14:47:11 +0800

    Initial Commit

---
+++ Assesment/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Assesment
+//
+//  Created by Edevane Tan on 17/12/2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ AssesmentTests/AssesmentTests.swift
@@ -0,0 +1,17 @@
+//
+//  AssesmentTests.swift
+//  AssesmentTests
+//
+//  Created by Edevane Tan on 17/12/2024.
+//
+
+import Testing
+@testable import Assesment
+
+struct AssesmentTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
